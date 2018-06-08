6e67a768d38cf0cbefc755a30dfb848ac949a8ba
MN <accm0ritz@gmail.com>
2018-06-08 09:23:26 +0200

    updated script.

--- Configs/Agent/conf_zbx_agent.sh
+++ Configs/Agent/conf_zbx_agent.sh
@@ -38,27 +38,3 @@ echo "zabbix_agent.sh config-file-path Hostname"
 exit
 fi
 exit
-
-
-mkdir -p /etc/zabbix/zabbix_agentd.d/
-
-sed -e '/LogFileSize=0/ s/^#*/#/' -i $1
-sed -e '/Server=127.0.0.1/ s/^#*/#/' -i $1
-sed -e '/ServerActive=127.0.0.1/ s/^#*/#/' -i $1
-sed -e '/Hostname=Zabbix server/ s/^#*/#/' -i $1
-echo >> $1
-echo >> $1
-echo >> $1
-echo "Include=/etc/zabbix/zabbix_agentd.d/" >> $1
-echo "LogFileSize=10" >> $1
-echo "Timeout=15" >> $1
-echo >> $1
-echo "Server=zabbix.aixit.com" >> $1
-echo "ServerActive=zabbix.aixit.com" >> $1
-echo "Hostname=$2" >> $1
-/etc/init.d/zabbix-agent restart
-systemctl restart zabbix-agent.service
-echo "Finished"
-#echo "You must edit Hostname!!"
-echo
-
